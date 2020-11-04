c1410b7d891fc443648827a50918fe582d09b8f0
halan1999 <67534299+halan1999@users.noreply.github.com>
2020-11-05 00:48:29 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const aws = require("aws-sdk");
 
 const dynamoDB = new aws.DynamoDB.DocumentClient({
-  region: "ap-southeast-1",
-  accessKeyId: "AKIAQ3QLWOE2QGA4MY7Z",
-  secretAccessKey: "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",
+  region: "",
+  accessKeyId: "",
+  secretAccessKey: "",
 });
 
 module.exports = dynamoDB;
